d5f35b7eb896d6dd50f35d0e69a9c2276d299b04
唐茄茄 <584231366@qq.com>
2018-08-13 22:12:15 +0800

    Signed-off-by: 唐茄茄 <584231366@qq.com>

---
+++ src/RoutesServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TJJLaravel\Routes;
+
+use Illuminate\Support\ServiceProvider;
+
+class RoutesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        echo 'RoutesServiceProvider';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
